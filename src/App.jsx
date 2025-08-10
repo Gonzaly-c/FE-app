afdd22bb4f11afa4d417cc98a0a830582dc6244a
Santiago Ferrero <santifnob@gmail.com>
2025-08-10 02:01:30 -0300

    Add register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { LoginPage } from "./auth/LoginPage.jsx"
 import { AuthProvider } from "./auth/AuthProvider.jsx"
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom"
+import { RegisterPage } from "./pages/RegisterPage.jsx"
 
 
 
@@ -9,13 +10,13 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         
-
         <Routes>
           
           <Route path='/admin/dashboard' element={<h1>Admin Home</h1>}></Route>
           <Route path='/conductor/dashboard' element={<h1>Conductor Home</h1>}></Route>
 
           <Route path='/' element={<LoginPage/>}></Route>
+          <Route path='/register' element={<RegisterPage/>}></Route>
         </Routes>
 
       </AuthProvider>
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,131 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+//import { registerUser } from "../services/api";
+
+export function RegisterPage() {
+  const { register, formState: {isSubmitting, isValid, errors} , handleSubmit} = useForm({
+    mode: "onBlur"
+  })
+  
+  const [form, setForm] = useState({
+    nombre: "",
+    apellido: "",
+    correo: "",
+    password: ""
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!form.nombre || !form.apellido || !form.correo || !form.password) {
+      alert("Por favor completa todos los campos.");
+      return;
+    }
+
+    try {
+      throw new Error();
+      // const response = await registerUser(form);
+      // if (response.success) {
+      //   alert("Cuenta creada con éxito");
+      //   navigate("/login");
+      // } else {
+      //   alert("Error al crear cuenta");
+      // }
+    } catch (error) {
+      alert("Hubo un problema con el registro");
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="container d-flex justify-content-center align-items-center min-vh-100">
+      <div className="card p-4" style={{ maxWidth: "400px", width: "100%" }}>
+        <h1 className="mb-4 text-center">Registrarse</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-1">
+            <label className="form-label" htmlFor="nombre">Nombre:</label>
+            <input id="nombre" type="text" {...register('nombre', {required: 'El nombre es requerido', pattern: {
+                value: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/,
+                message: "Solo se permiten letras y espacios"
+              }})}           
+              className="form-control" placeholder="Nombre"/>
+            {errors.nombre && <span className="text-danger">{errors.nombre.message}</span>}
+          </div>
+
+          <div className="mb-1">
+            <label className="form-label" htmlFor="apellido">Apellido:</label>
+            <input id="apellido" type="text" {...register('apellido', {required: 'El apellido es requerido',
+              pattern: {
+                value: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/,
+                message: "Solo se permiten letras y espacios"
+              }
+            })} 
+              className="form-control" placeholder="Apellido"/>
+            {errors.apellido && <span className='text-danger'>{errors.apellido.message}</span>}
+          </div>
+
+          <div className="mb-1">
+            <label htmlFor="correo" className="form-label">Correo electrónico:</label>
+            <input id="correo" {...register('email', {required: "El email es requerido", pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "Invalid email address"}})
+              } className="form-control" placeholder="Correo electrónico"/>
+          </div>
+          {errors.email && <span className='text-danger'>{errors.email.message}</span>}
+        
+          <div className="mb-2">
+            <label htmlFor="password" className="form-label">Contraseña:</label>
+            <input type="password" id="password" {...register('password', {required: "La contraseña es requerida", })} className="form-control" placeholder="Contraseña"/>
+            {errors.password && <span className='text-danger'>{errors.password.message}</span>}
+          </div>
+
+          <div className="d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={() => navigate("/login")}
+          >
+            Volver
+          </button>
+          <button type="submit" className="btn btn-success">
+            Crear cuenta
+          </button>
+        </div>
+        
+        </form>
+
+        <div className="mt-4 border-top pt-3 text-center">
+          <p>Registrarse con</p>
+          <div className="d-flex justify-content-center gap-3">
+            <img
+              src="https://img.icons8.com/color/48/google-logo.png"
+              alt="Google"
+              style={{ width: "35px", cursor: "pointer" }}
+            />
+            <img
+              src="https://img.icons8.com/color/48/facebook-new.png"
+              alt="Facebook"
+              style={{ width: "35px", cursor: "pointer" }}
+              className="mx-2"
+            />
+            <img
+              src="https://img.icons8.com/ios-filled/50/mac-os.png"
+              alt="Apple"
+              style={{ width: "35px", cursor: "pointer" }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
