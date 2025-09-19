d6935377bfe16e45ef8cf85158c63d09a8027970
Gonzaly <constante.gonzalo2@gmail.com>
2025-09-19 20:25:43 -0300

    Recorrido CRUD

---
+++ src/hooks/recorrido/useRecorridosQuery.js
@@ -0,0 +1,13 @@
+import { api } from '../../services/api'
+import { useQuery } from '@tanstack/react-query'
+
+export function useTrenesQuery () {
+  return (useQuery({
+    queryKey: ['trenesQuery'],
+    queryFn: async () => {
+      const res = await api.get('/tren', { withCredentials: true })
+      return res.data.data
+    }
+  }))
+}
+
